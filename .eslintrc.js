d56636592ff920a82630d699837fec8f93fd1256
Nathakorn Chaninthanadecha <nathakorn.tonch@gmail.com>
2022-03-13 05:07:06 +0700

    Create personal ESLint config

---
+++ .eslintrc.js
@@ -0,0 +1,57 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true,
+    'react-native/react-native': true,
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  plugins: [
+    '@typescript-eslint',
+    'jest',
+    'react',
+    'react-hooks',
+    'react-native',
+    'unused-imports',
+    'prettier',
+  ],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:import/recommended',
+    'plugin:import/typescript',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:react/jsx-runtime',
+    'plugin:prettier/recommended',
+  ],
+  rules: {
+    '@typescript-eslint/no-var-requires': 'off',
+    '@typescript-eslint/no-unused-vars': 'off',
+    'jest/no-disabled-tests': 'warn',
+    'jest/no-focused-tests': 'error',
+    'jest/no-identical-title': 'error',
+    'jest/prefer-to-have-length': 'warn',
+    'jest/valid-expect': 'error',
+    'unused-imports/no-unused-imports': 'error',
+    'unused-imports/no-unused-vars': [
+      'warn',
+      {
+        vars: 'all',
+        varsIgnorePattern: '^_',
+        args: 'after-used',
+        argsIgnorePattern: '^_',
+      },
+    ],
+  },
+}
--- README.md
+++ README.md
@@ -1,2 +1,31 @@
 # eslint-config-nijicha
+
 Personal ESLint rules for React Project (with Typescript ❤️)
+
+## Installation
+
+Install npm package and its peerDependencies
+
+```shell
+yarn add -D eslint-config-nijicha
+npx install-peerdeps --dev eslint-config-nijicha
+```
+
+## Usage
+
+1. Add config to your `.eslintrc.js`
+
+```
+{
+  'extends': [
+    'nijicha'
+  ]
+}
+```
+
+2. Copy `.husky/pre-commit` and `.prettierrc` into your project
+
+## TODO
+
+- [ ] Add development section
+- [ ] Add script to copy setting to the setting up project
